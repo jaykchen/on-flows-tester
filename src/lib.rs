4943644b0346509be156d614996011189abf0733
Ji Chen <jaykchen@gmail.com>
2024-03-07 23:55:09 -0500

    http_req

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,49 @@ async fn handler(body: Vec<u8>) {
     let _ = inner().await;
 }
 async fn inner() -> anyhow::Result<()> {
-    let octocrab = get_octo(&GithubLogin::Default);
-    let report_issue_handle = octocrab.issues("pytorch", "pytorch");
+    let _ = github_http_post_issue_comment("hard coded comment by flows").await?;
+    // let octocrab = get_octo(&GithubLogin::Default);
+    // let report_issue_handle = octocrab.issues("pytorch", "pytorch");
 
-    let report_issue = report_issue_handle.create_comment(1329, "hard coded comment by flows").await?;
+    // let report_issue = report_issue_handle.create_comment(1329, "hard coded comment by flows").await?;
     // let report_issue_number = report_issue.number;
     // let label_slice = vec!["fake".to_string()];
     // let _ = report_issue_handle.update(report_issue_number).labels(&label_slice).send().await?;
 
     Ok(())
+}
+pub async fn github_http_post_issue_comment(query: &str) -> anyhow::Result<Vec<u8>> {
+    dotenv().ok();
+
+    use http_req::{request::Method, request::Request, uri::Uri};
+    let token = std::env::var("GITHUB_TOKEN").expect("github_token is required");
+
+    // let base_url = "https://api.github.com/repos/{owner}/{repo}/issues/{issue}/comments";
+    let base_url = "https://api.github.com/repos/pytorch/pytorch/issues/1329/comments";
+
+    let base_url = Uri::try_from(base_url).unwrap();
+    let mut writer = Vec::new();
+
+    let query = serde_json::json!({"body": query});
+    match Request::new(&base_url)
+        .method(Method::POST)
+        .header("User-Agent", "flows-network connector")
+        .header("Content-Type", "application/json")
+        .header("Authorization", &format!("Bearer {}", token))
+        .header("Content-Length", &query.to_string().len())
+        .body(&query.to_string().into_bytes())
+        .send(&mut writer)
+    {
+        Ok(res) => {
+            if !res.status_code().is_success() {
+                log::error!("Github http error {:?}", res.status_code());
+                return Err(anyhow::anyhow!("Github http error {:?}", res.status_code()));
+            }
+            Ok(writer)
+        }
+        Err(_e) => {
+            log::error!("Error getting response from Github: {:?}", _e);
+            Err(anyhow::anyhow!(_e))
+        }
+    }
 }
